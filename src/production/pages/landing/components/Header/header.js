85374916771586a79c347c88bba169911f54c7f2
therealgabryx <gabriele.concli@gmail.com>
2020-10-09 23:44:06 +0000

    modified header

--- src/production/pages/landing/components/Header/header.js
+++ src/production/pages/landing/components/Header/header.js
@@ -6,11 +6,11 @@ class Header extends React.Component {
     return (
       <React.Fragment>
         <header className="header">
-          <nav className={"navbar navbar-expand-lg fixed-top" + (this.props.bgColor && this.props.bgColor === 'white' ? 'custom-nav white-bg' : 'bg-transparent')}>
+          <nav className={"navbar navbar-expand-lg fixed-top " + (this.props.bgColor && this.props.bgColor === 'white' ? 'custom-nav white-bg' : 'bg-transparent')}>
             <div className="container">
-              <a className="navbar-brand" href="/">
+              <a className="navbar-brand" href="/template">
                 <img
-                  src={this.props.bgColor && this.props.bgColor === 'white' ? "img/logo-color-1x.png" : "img/logo-white-1x.png"}
+                  src={this.props.bgColor && this.props.bgColor === 'white' ? "../img/logo-color-1x.png" : "../img/logo-white-1x.png"}
                   width="120"
                   alt="logo"
                   className="img-fluid"
@@ -36,7 +36,7 @@ class Header extends React.Component {
                   <li className="nav-item dropdown">
                     <a
                       className="nav-link page-scroll dropdown-toggle"
-                      href="/#"
+                      href="/template#"
                       id="navbarDropdownHome"
                       role="button"
                       data-toggle="dropdown"
@@ -49,22 +49,22 @@ class Header extends React.Component {
                       className="dropdown-menu submenu"
                       aria-labelledby="navbarDropdownHome"
                     >
-                      <a className="dropdown-item" href="/theme1">
+                      <a className="dropdown-item" href="/template/theme1">
                         Demo Template 1
                       </a>
-                      <a className="dropdown-item" href="/theme2">
+                      <a className="dropdown-item" href="/template/theme2">
                         Demo Template 2
                       </a>
-                      <a className="dropdown-item" href="/theme3">
+                      <a className="dropdown-item" href="/template/theme3">
                         Demo Template 3
                       </a>
-                      <a className="dropdown-item" href="/theme4">
+                      <a className="dropdown-item" href="/template/theme4">
                         Demo Template 4
                       </a>
-                      <a className="dropdown-item" href="/theme5">
+                      <a className="dropdown-item" href="/template/theme5">
                         Demo Template 5
                       </a>
-                      <a className="dropdown-item" href="/theme6">
+                      <a className="dropdown-item" href="/template/theme6">
                         Demo Template 6
                       </a>
                     </div>
@@ -80,54 +80,23 @@ class Header extends React.Component {
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link page-scroll" href="#pricing">
-                      Pricing
-                    </a>
-                  </li>
-                  <li className="nav-item">
                     <a className="nav-link page-scroll" href="#screenshots">
                       Screenshots
                     </a>
-                  </li>
+                  </li>               
+                                                                              
                   <li className="nav-item dropdown">
-                      <a className="nav-link page-scroll dropdown-toggle" href="/#" id="navbarDropdownPage" role="button"
-                          data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          Pages <span className="custom-nav-badge badge badge-danger badge-pill">New</span>
-                      </a>
-                      <div className="dropdown-menu submenu" aria-labelledby="navbarDropdownPage">
-                          <a className="dropdown-item" href="login1">Login Page 1</a>
-                          <a className="dropdown-item" href="login2">Login Page 2</a>
-                          <a className="dropdown-item" href="signup1">Signup Page 1</a>
-                          <a className="dropdown-item" href="signup2">Signup Page 2</a>
-                          <a className="dropdown-item" href="resetPassword">Password Reset</a>
-                          <a className="dropdown-item" href="changePassword">Change Password</a>
-                          <a className="dropdown-item" href="download">Download Page</a>
-                          <a className="dropdown-item" href="review">Review Page</a>
-                          <a className="dropdown-item" href="faq">FAQ Page</a>
-                          <a className="dropdown-item" href="404">404 Page</a>
-                          <a className="dropdown-item" href="comingSoon">Coming Soon</a>
-                          <a className="dropdown-item" href="thankyou">Thank You</a>
-                          <a className="dropdown-item" href="team">Team Page</a>
-                          <a className="dropdown-item" href="singleTeam">Team Single</a>
-                      </div>
-                  </li>
-                  <li className="nav-item dropdown">
-                        <a className="nav-link page-scroll dropdown-toggle" href="/#" id="navbarBlogPage" role="button"
+                        <a className="nav-link page-scroll dropdown-toggle" href="/template#" id="navbarBlogPage" role="button"
                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Blog <span className="custom-nav-badge badge badge-danger badge-pill">New</span>
                         </a>
 
                         <div className="dropdown-menu submenu" aria-labelledby="navbarBlogPage">
-                            <a className="dropdown-item" href="blog">Blog Grid</a>
-                            <a className="dropdown-item" href="blogLeft">Blog Left Sidebar</a>
-                            <a className="dropdown-item" href="blogRight">Details Right Sidebar</a>
+                            <a className="dropdown-item" href="/template/blog">Blog Grid</a>
+                            <a className="dropdown-item" href="/template/blogLeft">Blog Left Sidebar</a>
+                            <a className="dropdown-item" href="/template/blogRight">Details Right Sidebar</a>
                         </div>
-                    </li>
-                  <li className="nav-item">
-                    <a className="nav-link page-scroll" href="#team">
-                      Team
-                    </a>
-                  </li>
+                    </li>                                                 
                   <li className="nav-item">
                     <a className="nav-link page-scroll" href="#contact">
                       Contact
